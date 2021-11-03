902fd773b187838ff7c6212ac6876154bc4facb7
apple-yagi <applesryu@gmail.com>
2021-11-03 23:32:00 +0900

    Add VPokemonCard.vue

---
+++ components/VPokemonCard.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="card">
+    <img class="card__img" :src="pokemon.image_s" :alt="pokemon.name" />
+    <div class="card__content">
+      <h3 class="card__title">
+        {{ pokemon.name }}
+      </h3>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { PropType } from "vue";
+import { Pokemon } from "~~/types";
+
+export default defineComponent({
+  props: {
+    pokemon: {
+      type: Object as PropType<Pokemon>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.card {
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  border: 1px solid #888;
+  border-radius: 20px;
+  box-shadow: 0 3px 6px -2px #000a3b34;
+
+  &__img {
+    width: 100%;
+    height: auto;
+  }
+
+  &__content {
+    flex: 1;
+  }
+
+  &__title {
+    font-size: 16px;
+  }
+}
+</style>
--- pages/pokemons.vue
+++ pages/pokemons.vue
@@ -4,19 +4,31 @@ const { data: pokemonsResponse } = await useFetch("/api/pokemons");
 
 <template>
   <main class="main">
-    <ul>
-      <li v-for="(pokemon, i) in pokemonsResponse.results" :key="i">
-        <img :src="pokemon.image_m" :alt="pokemon.name" />
-        {{ pokemon.name }}
-      </li>
-    </ul>
+    <div class="container">
+      <v-pokemon-card
+        v-for="(pokemon, i) in pokemonsResponse.results"
+        :key="i"
+        :pokemon="pokemon"
+      />
+    </div>
   </main>
 </template>
 
-<style scoped>
-.main {
+<style lang="scss" scoped>
+.container {
   max-width: 960px;
   min-height: calc(100vh - 100px);
   margin: 0 auto;
+  display: grid;
+  grid-template-columns: repeat(1, 1fr);
+  grid-gap: 2em 1.8em;
+
+  @media screen and (min-width: 768px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media screen and (min-width: 1024px) {
+    grid-template-columns: repeat(6, 1fr);
+  }
 }
 </style>
